a5d79be529c0242c38b142fee127db2470bbffe4
bhuvan-dv <damavenkateshraj.b@northeastern.edu>
2025-06-24 23:35:22 -0400

    operators types and string methods

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 // defining variables let a;
 // initializing variables let a=5;
-let a = 5, b=6
+let a = 5, b = 6
 // + is a operator
-console.log(a+b)
+console.log(a + b)
 
 // block is a comprised set of instructions
 {
-    console.log(a-b);
+    console.log(a - b);
 }
 // single line comment
 /*
@@ -19,8 +19,8 @@ multi line comment
 // 1. Cannot start with a number
 // 2. Except $ and _ no special characters are allowed
 // 3. we cannot use keywords as identifiers
-function sum(a, b){
-    return a+b;
+function sum(a, b) {
+    return a + b;
 }
 // function is a keyword
 // add, sum is a identifiers - user defined
@@ -28,19 +28,20 @@ function sum(a, b){
 // return is a keyword
 // + is a operator 
 // ; is a token
-let d=78;
+let d = 78;
 {
-    let c=89; //scope variable - block variable
+    let c = 89; //scope variable - block variable
 }
 console.log(d);
 
-let replay="pause"
+let replay = "pause"
 
-function restart(){
+function restart() {
+    let boat;
     return replay
 }
 restart()
-let f= "abcd" //! NEVER USE KEYWORDS AS IDENTIFIERS
+let f = "abcd" //! NEVER USE KEYWORDS AS IDENTIFIERS
 //  let and function is a keyword
 // c d replay restart are identifiers
 // return and console are keywords
@@ -48,7 +49,7 @@ let f= "abcd" //! NEVER USE KEYWORDS AS IDENTIFIERS
 // anything with { } is called block
 
 // Literals: Data or values which are used for variables 
-let t=true
+let t = true
 
 // Datatypes in Javascript
 /*
@@ -94,8 +95,8 @@ console.log(typeof Object); //Object
 // let k; 
 // console.log(k);
 
-let name="Abhinav"
-let age=18
+let name = "Abhinav"
+let age = 18
 // template literals
 console.log(`${name} is of age ${age}`);
 
@@ -108,6 +109,221 @@ console.log(`${name} is of age ${age}`);
 // console.log(string2.toUpperCase());
 
 // WAP to input firstname and lastname and then join both and print as whole name
-let fname=prompt("enter first name")
-let lname=prompt("enter last name")
-console.log(`${fname} ${lname}`);
+// let fname=prompt("enter first name")
+// let lname=prompt("enter last name")
+// console.log(`${fname} ${lname}`);
+
+//WAP to accept any string as input along with the index to print characters present in that position
+// let strp = prompt("enter the string");//abhinav
+// let index = prompt("enter the index");
+// console.log(strp[index]);
+// console.log(strp.charAt(index));
+
+//WAP to accept a string and a character present in that string and return its index
+// let strp= "hello"
+// let character= prompt("enter the character");
+// console.log(strp.charCodeAt(0));
+// console.log(strp.charCodeAt(1));
+// console.log(strp.charCodeAt(2));
+// console.log(strp.charCodeAt(3));
+//! returns unicode value of the character you pass
+
+//WAP to join two strings
+let hello = "hello"
+let world = "world"
+console.log(hello.concat(world));
+
+//WAP to check whether a string ends with a specified substring
+let question = "Hey did you do your homework?"
+console.log(question.endsWith("ork?"));
+
+//WAP to check if a string includes any substring or character?
+console.log(question.includes("did"));
+
+//WAP to cut a portion of the string 
+let substr = question.slice(0, 7);
+console.log(substr);
+
+let substr2 = question.substring(0, 7)
+console.log(substr);
+// Assignment find the difference b/w substring and slice
+
+
+/* Operators in JS
+=========================
+1. Airthmetic - are used only on datatype number
+    + addition
+    - subtraction
+    * multiplication
+    / division
+    % modulus
+    ++ increment
+    -- decrement
+Examples
+    
+
+2. Logical 
+    && - and
+    || - OR
+    ! - not
+--------------AND logic -------------------
+    0 -stands for False
+    1 - stands for true
+    input       output
+    T   T       T
+    rest all the time the output is False
+--------------OR logic -------------------
+    0 -stands for False
+    1 - stands for true
+    input       output
+    F   F       F
+    rest all the time the output is True
+--------------Not logic -------------------
+    !false = true
+    !0 = 1
+3. Assignment 
+    symbol      Operator
+    =           Assignment
+    +=          Additional assignment
+    -=          Subtractal assignment
+    *=          multiplicational assignment
+    /=          Divisional assignment
+    %=          modulus assignment
+4. Conditional
+if x==2{
+    //true part
+    }else{
+        //false part
+}
+Ternary operator ( condition ? 'true' : 'false' )
+(x==2)? "true part" : "false part"
+5. Comparison
+Symbol  Function
+==      Equal to or not - it checks only the value
+===     Strict equal to - it checks both the value and the datatype
+!=      Not equal to - only the value
+!==     Strict not equal - both value and datatype
+>       Greater than
+<       less than
+>=      Greater or equal to
+<=      lesser than or equal to
+6. Concatination
++       concats two strings
+*/
+
+console.log("==========Airthmetic Op examples========");
+console.log(4 + 3);//7
+console.log(4 - 3);//1
+console.log(4 * 3);//12
+console.log(4 / 3);//1
+console.log(4 % 3);//1
+let y = 5
+console.log("pre and post");
+// Post and pre 
+// sign variable = pre => in the memory before storing the value of y it will increment
+// varaible sign = post => it will store the current value and when called for an operation it will update the value
+console.log(++y);//6
+console.log(--y);//5
+console.log(y++);//5 -> 6
+console.log(y--);//6 
+console.log(y);//5
+console.log("==========Logical Op examples========");
+console.log("AND && gate");
+console.log(false && false);//false
+console.log(false && true);//false
+console.log(true && false);//false
+console.log(true && true);//true
+console.log("OR || gate");
+console.log(false || false);//false
+console.log(false || true);//true
+console.log(true || false);//true
+console.log(true || true);//true
+console.log("NOT ! gate");
+console.log(!false); //true
+console.log(!true); //false
+console.log("==========Assignment Op examples========");
+let x = 25 //= assignment operator
+console.log(x += 5); //+= add assignment operator //output 30 //x=x+5
+console.log(x -= 5); //+= sub assignment operator //output 25 //x=x-5
+console.log(x *= 5); //+= multiplication assignment operator //output 125 //x=x*5
+console.log(x /= 5); //+= divisional assignment operator //output 25 //x=x/5
+console.log(x %= 5); //+= modulus assignment operator //output 0 //x=x%5
+console.log("==========Conditional Op examples========");
+console.log(x += 5);
+// (Expression) ? "True" : "False"
+(x == 5) ? console.log("x is 5") : console.log("x is not 5");
+console.log("==========Comparison Op examples========");
+let w = 56
+let z = 56
+console.log(w == z);//true
+console.log(w != z);//false
+console.log(w === z);//true
+console.log(w !== z);//false
+console.log(w < z);//false
+console.log(w > z);//false
+console.log(w >= z);//true
+console.log(w <= z);//true
+//come back to this after type coercion
+console.log("come back to this after type coercion");
+w=56
+z="56"
+console.log(w == z);//true
+console.log(w != z);//false
+console.log(w === z);//false
+console.log(w !== z);//true
+console.log(w < z);//false
+console.log(w > z);//false
+console.log(w >= z);//true
+console.log(w <= z);//true
+console.log("==========Concatination Op examples========");
+let ab = "ab"
+let cd = "cd"
+console.log(ab + cd);//abcd
+
+
+console.log("==========NEW TOPIC========");
+console.log("==========Type coercion examples========");
+// Definition of type coercion - Conversion of one type of data into a another type automatically by the JS engine is called as TC
+/* Rules of type coercion
+1. In JS every number other than 0 is respresented as boolean value true
+2. 0,null, NaN, "", undefined all these represent boolean value false
+3. Any non empty string is considered as true.
+*/
+console.log(5 - "1");//4
+console.log(5 + Number("1"));//51
+console.log(5 - "a");//5-a - NaN
+console.log(5 + "a");//5+a - 5a
+console.log(5 * "a");//5*a - NaN
+console.log(5 / "a");//5/a - NaN
+
+
+//logical || behaves slightly differently instead returning true it returns the first positive value if both are true
+// it will return second value if any one value or both the values are false
+console.log(10 || 20);//true
+console.log(0 || 20);//20
+console.log(-10 || -20);//-10
+
+// WAP to print 0 if the count of items is `empty`. 
+// If the count is non-zero display the count
+let count = 0
+count != 0 ? console.log(count) : console.log(!count);
+
+// Explicit type casting
+let fi = "1"
+console.log(typeof fi);
+console.log(typeof Number(fi));
+
+let l = "a"
+console.log(Number(l));//NaN
+let stry = "Abhinav"
+console.log(Number(stry));//NaN
+
+//WAP to take two numbers as input from the user and log the sum on the screen
+// console.log(Number(prompt("give num1"))+Number(prompt("give num1")));
+
+//WAP to take two string and concatinate them
+// console.log(prompt("give st1")+prompt('give str2'));
+
+//WAP to input the age and check if they are eligible to drive 
+let age2 = Number(prompt("input age"));
+age2 >= 16 ? console.log("you can drive") : console.log("you can't drive just walk");
