b7f63691d801db0d30a8f5daf730057c2fc4b253
bhuvan-dv <damavenkateshraj.b@northeastern.edu>
2025-07-04 19:16:11 -0400

    commented function code

--- js/module.js
+++ js/module.js
@@ -1,18 +1,18 @@
 //!Functions in Javascript
 //WAFP to add two numbers
 
-function sum(x,y){
-    return x+y;
-}
-console.log(sum(3,4));
+// function sum(x,y){
+//     return x+y;
+// }
+// console.log(sum(3,4));
 
 //functionkeyword functionName (input params){
 // return statement;
 // }
 
 //WAFP to find the subtraction of two numbers
-function subtraction (x,y){
-    return x-y
-}
-console.log(subtraction(10,3));
+// function subtraction (x,y){
+//     return x-y
+// }
+// console.log(subtraction(10,3));
 
